c23acf38d5e5ff592bf102cf59eacb913cdb9604
Kyle Burke <kyle.alex.burke@gmail.com>
2014-12-04 22:47:13 -0700

    dunno

--- node.js
+++ node.js
@@ -2,9 +2,13 @@ var express = require('express');
 var app = express();
 
 app.use(express.static(__dirname + '/public'));
-
-app.listen(process.env.PORT || 3000);
+app.set('port', (process.env.PORT || 5000))
 
 app.get('/', function(req, res) {
   res.sendFile('index.html');
 });
+
+app.listen(app.get('port'), function() {
+  console.log('listening on: ' + app.get('port'));
+});
+
