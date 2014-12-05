152495d5c21681d951e8bd9444c88dd0837ed490
Kyle Burke <kyle.alex.burke@gmail.com>
2014-12-04 22:42:39 -0700

    first

---
+++ public/js/app.js
@@ -0,0 +1,93 @@
+/**
+ * Created by kyle on 12/4/14.
+ */
+(function() {
+  angular.module("home", ['firebase'])
+    .controller("HomeController", ['$scope', '$firebase', '$http',
+      function($scope, $firebase, $http) {
+        var ref = new Firebase("https://brilliant-torch-8918.firebaseio.com/");
+        var sync = $firebase(ref);
+
+        $scope.messages = sync.$asArray();
+        $scope.username = "";
+        $scope.profilePic = "/img/profile.png";
+        $scope.activeUserPic = "";
+        $scope.loggedIn = false;
+
+
+        $scope.addMessage = function(e) {
+          if ($scope.msg) {
+            $scope.messages.$add({
+              name: $scope.username || 'Anonymous',
+              description: $scope.msg,
+              img: $scope.activeUserPic
+            });
+
+            $scope.msg = "";
+
+          }
+        };
+
+        $scope.expandTextArea = function($event) {
+          console.log($event);
+          $event.target.setAttribute('rows', '3');
+          console.log(angular.element('#msgtextarea'));
+          angular.element('#msg-submit').css('display', 'inline');
+          angular.element('.glyphicon-camera').css('display', 'inline');
+
+        };
+
+        $scope.shrinkTextArea = function($event) {
+          if (!$scope.msg) {
+            $event.target.setAttribute('rows', '1');
+            angular.element('#msg-submit').css('display', 'none');
+            angular.element('.glyphicon-camera').css('display', 'none');
+          }
+        };
+
+        $scope.triggerOauth = function() {
+          ref.authWithOAuthPopup("google", function(error, authData) {
+            console.log('GOT THAT AUTHDATA: ');
+            console.log(authData);
+            $scope.username = authData.google.displayName;
+            $scope.profilePic = authData.google.cachedUserProfile.picture;
+            $scope.activeUserPic = authData.google.cachedUserProfile.picture;
+
+            $http({
+              url: 'https://api.imgur.com/3/image',
+              method: 'POST',
+              header: {
+                Authorization: 'Client-ID 92a47c218f82f09'
+              },
+              params: {
+                image: authData.google.cachedUserProfile.picture
+              }
+            })
+              .success(function(d, s, h, c) {
+                console.log(d);
+                console.log(s);
+                console.log(h);
+                console.log(c);
+              })
+              .error(function(d, s, h, c) {
+                console.log(d);
+                console.log(s);
+                console.log(h);
+                console.log(c);
+              });
+
+            $scope.loggedIn = true;
+            $scope.$apply();
+          }, {
+            remember: "sessionOnly"
+          });
+        }
+    }])
+    .filter('reverse', function() {
+      return function(items) {
+        return items.slice().reverse();
+      }
+    });
+
+
+})();
